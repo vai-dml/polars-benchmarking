31b3b2a491a89e91e2ceda560ac64e3890e60b96
Vaibhav <vaibhav@cldcvr.com>
2023-05-12 13:20:58 +0800

    added pandas 2.0 compariosn

--- graph_plot.py
+++ graph_plot.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-def comparison_plot(pd_time, pl_time, title, unit = 'ms'):
-    name = ['Pandas', 'Polars']
-    values = [pd_time, pl_time]
+def comparison_plot(pd_time, pyarrow_time, pl_time, title, unit = 's'):
+    name = ['Pandas 1.0','Pandas 2.0','Polars']
+    values = [pd_time, pyarrow_time, pl_time]
     
     plt.subplots(figsize=(6,3))
     plt.grid()
     plt.rc('axes', axisbelow=True)
-    plt.barh(name, values, color=['#f84a4f','#339e49'], edgecolor='#666', height=.7)
+    plt.barh(name, values, color=['#f84a4f','#339e49', '#006cff'], edgecolor='#666', height=.7)
     plt.title(title)
     plt.xlabel(f"Average Time ({unit})")
     plt.show()
-    plt.savefig('img/comparison.png')
+    #plt.savefig('img/comparison.png')
--- pandas_polars_comparison.py
+++ pandas_polars_comparison.py
@@ -7,23 +7,27 @@ import time
 
 csv_file = '/Users/vaibhavkapur/Downloads/archive/Parking_Violations_Issued_-_Fiscal_Year_2017.csv'
 
+# reading data with pandas-backed 
+pd_start = time.time()
+df_pd = pd.read_csv(csv_file)
+pd_end = time.time()
+pd_time = pd_end - pd_start
+
+# reading data with pyarrow-backed nullable dtypes
+pyarrow_start = time.time()
+df_pd = pd.read_csv(csv_file,dtype_backend='pyarrow', engine='pyarrow')
+pyarrow_end = time.time()
+pyarrow_time = pyarrow_end - pyarrow_start
+
 # reading data with polars - Lazyframe
 pl_start = time.time()
 df_pl = pl.scan_csv(csv_file,ignore_errors = True)
 result_df = df_pl.collect()
-size = result_df.estimated_size()
 pl_end = time.time()
 pl_time = pl_end - pl_start
 
-# reading data with pandas
-pd_start = time.time()
-df_pd = pd.read_csv(csv_file,on_bad_lines='skip')
-pd_end = time.time()
-pd_time = pd_end - pd_start
-
-print(size)
 # plot
-comparison_plot(pd_time,pl_time, "Comparion between Pandas and Polars execution time")
+comparison_plot(pd_time, pyarrow_time, pl_time, "Comparion between Pandas and Polars execution time")
 
 
 
