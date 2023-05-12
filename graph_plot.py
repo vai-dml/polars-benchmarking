0e2f4b5a7143c56b0f03b308366df09a4bd942c6
Vaibhav <vaibhav@cldcvr.com>
2023-05-12 13:47:30 +0800

    updated the graph dimensions

--- graph_plot.py
+++ graph_plot.py
@@ -4,7 +4,7 @@ def comparison_plot(pd_time, pyarrow_time, pl_time, title, unit = 's'):
     name = ['Pandas 1.0','Pandas 2.0','Polars']
     values = [pd_time, pyarrow_time, pl_time]
     
-    plt.subplots(figsize=(6,3))
+    plt.subplots(figsize=(8,4))
     plt.grid()
     plt.rc('axes', axisbelow=True)
     plt.barh(name, values, color=['#f84a4f','#339e49', '#006cff'], edgecolor='#666', height=.7)
